94d0fad4450ae1099636ca0ce6f4df7b938ec3ff
Lior <liormoshe0909@gmail.com>
2017-01-18 16:01:18 +0200

    Upload client directory.
    These are the classes which we use most in the implementation of the client.

---
+++ client/Client.cpp
@@ -0,0 +1,146 @@
+#include "Client.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#define BUFFSIZE (32768 * 16)
+
+using namespace std;
+using namespace boost::archive;
+
+Client::Client(int argc, char* argv[])
+{
+	// Create a socket to run on the entered id adress and port.
+	// because it's not a server, no need to bind.
+ 	mySocket = new Tcp(0, atoi(argv[2]),argv[1]);
+ 	//Set all pointers to be NULL at first.
+	driver = NULL;
+	cab = NULL;
+	trip = NULL;
+	//Initialize the socket.
+	mySocket->initialize();
+
+	//Start creating the driver and communication process.
+	buildDriver();
+}
+
+Client::~Client()
+{
+	//Delete driver and cab.The driver will delete the trip by itself.
+	delete driver;
+	delete cab;
+	//Call socket's destructor to close it.
+	delete mySocket;
+}
+
+void Client::run()
+{
+	//Run a switch on the action that we will get from the server.
+	char action = -1;
+	do
+	{
+		//Receive an action from the server which is one byte.
+		mySocket->reciveData(&action, 1);
+		switch (action)
+		{
+			case moveOneStep:
+				//Server moved the driver one step so our driver will move as well.
+				driver->moveOneStep();
+				break;
+			case setTrip:
+				//Server tells us it will send us a new trip,run getTripFromServer function.
+				getTripFromServer();
+				break;
+			case shutDown:
+				//Server told us to shutDown so break from this switch.
+				break;
+			default:
+				// no such operator
+				break;
+		}
+	} while (action != shutDown);
+	//ACK tell server we got everything.
+	mySocket->sendData('a');
+}
+
+void Client::buildDriver()
+{
+	string serial_str;
+	int id, cabId, age, yearsOfExperience, bytes;
+	char buffer[BUFFSIZE], stat;
+	
+	//-------------------------get driver's members as input---------------
+	scanf("%d,%d,%c,%d,%d", &id, &age, &stat, &yearsOfExperience, &cabId);
+	driver = new Driver(id, age, stat, yearsOfExperience, NULL, NULL);
+
+	//---------------------------serializing the driver--------------------
+	this->serializeObj<Driver>(&serial_str, driver);
+
+	// Send serialized string through the socket.
+	mySocket->sendData(serial_str);
+
+	// Now the client will wait to receive the cab from the server.
+	bytes = mySocket->reciveData(buffer, BUFFSIZE);
+	cab = deSerializeObj<Cab>(buffer, bytes);
+	driver->setCab(cab);
+	// Get action of setTrip from server before setting the trip.
+	mySocket->reciveData(buffer, 1);
+	//Get the trip from the server.
+	getTripFromServer();
+}
+
+void Client::getTripFromServer()
+{
+	// Now the client will wait to receive the trip from the server.
+	char buffer[BUFFSIZE];
+	int bytes = mySocket->reciveData(buffer, BUFFSIZE);
+	// -1 because the socket added '\0' at the end
+	trip = deSerializeObj<Trip>(buffer, bytes);
+	driver->setTrip(trip);
+}
+
+
+template<class T>
+void Client::serializeObj(std::string* serial_str, T* obj)
+{
+	//Serialize the object using recitation examples.
+	boost::iostreams::back_insert_device<std::string> inserter(*serial_str);
+	boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(
+			inserter);
+	boost::archive::binary_oarchive oa(s);
+	oa << obj;
+	s.flush();
+}
+
+template<class T>
+T* Client::deSerializeObj(const char* serial_str, int size)
+{
+	T* obj;
+	//Deserialize same as recitation examples.
+	boost::iostreams::basic_array_source<char> device(serial_str, size);
+	boost::iostreams::stream<boost::iostreams::basic_array_source<char> > s(
+			device);
+	boost::archive::binary_iarchive ia(s);
+	ia >> obj;
+	return obj;
+}
+
+int main(int argc, char* argv[])
+{
+	//Create a driver.
+	Client driver(argc, argv);
+	//Get commands from server in run function
+	driver.run();
+	return 0;
+}
---
+++ client/Client.h
@@ -0,0 +1,84 @@
+#ifndef CLIENT_MAINFLOW_H_
+#define CLIENT
+
+#include <string>
+#include "../cabs/CabFactory.h"
+#include "../server/TaxiCenter.h"
+#include "../map/Map.h"
+#include "../sockets/Tcp.h"
+#include "../sockets/Socket.h"
+
+class Map;
+class CabFactory;
+class TaxiCenter;
+class ClientInput;
+/*
+ * This is our Client class,it is used as one side of the communication process
+ * between the client and the server in our program.The Client will create a driver according
+ * to the input that it got from the user,send it to the server and then get a cab and afterwards
+ * a trip from the server,it will shutDown when the server will tell him to do so.
+ */
+class Client
+{
+private:
+	//Socket that would be used as one side of the communication process.
+	Socket* mySocket;
+	//The driver this client will represent.
+	Driver* driver;
+	//This driver's cab(will be given from sever).
+	Cab* cab;
+	//The client's Trip,will be given from server as wll.
+	Trip* trip;
+public:
+	/*
+	 * Define an enum named Actions that will be used as commands that will be sent between
+	 * the Client and the Server during their communication process.
+	 */
+	typedef enum
+	{
+		moveOneStep = 0, setTrip = 1, endedTrip = 2, shutDown = 3
+	} Actions;
+	/*
+	 * Constructor of the Client class,will get the command line arguments which we will get in
+	 * the main function,using these arguments it will create a socket with the entered
+	 * ip adress and port.
+	 */
+	Client(int argc, char* argv[]);
+	/*
+	 * Destructor of client class.
+	 */
+	virtual ~Client();
+	/*
+	 * This method will get an input from the user and create the driver accordingly,afterwards
+	 * it will serialize the driver and send it to the server and then wait for the server's
+	 * response which will be the cab for this driver,this function starts the communication
+	 * with the server and runs until it ends.
+	 */
+	void buildDriver();
+	/*
+	 * This function will get the serialized trip from the server,deserialize it and set
+	 * it to be the trip which the driver this Client represents will hold.
+	 */
+	void getTripFromServer();
+	/*
+	 * This function will run a switch of commands from the Actions enum and will run a
+	 * different operation according to the Action the Server will send to this Client.
+	 */
+	void run();
+	/*
+	 * This serialize function will get an adress of a string and a pointer to some class
+	 * which we defined with a template and will serialize it with boost's serialization and
+	 * write the serialized string to the entered adress.
+	 */
+	template<class T>
+	void serializeObj(std::string* serial_str, T* obj);
+	/*
+	 * This function will get a pointer to a char array and it's size and will deserialize it
+	 * with boost's serialization library and return a pointer to the deserialized object,because
+	 * we want the function to be generic and deserialize a lot of classes we used a template.
+	 */
+	template<class T>
+	T* deSerializeObj(const char* serial_str, int size);
+};
+
+#endif /* CLIENT	*/
